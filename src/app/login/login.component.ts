c71b95eb6adbabcf2fbb0f06097f542370be802d
Amr-Elharery <amrelharery@gmail.com>
2024-07-29 18:46:27 +0300

    Finish login and logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,31 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, HttpClientModule],
+  providers:[AuthService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  constructor(private _AuthService: AuthService){}
 
+  loginForm: FormGroup = new FormGroup({
+    "email": new FormControl(null,[Validators.required, Validators.email]),
+    "password": new FormControl(null, [Validators.required, Validators.minLength(8)])
+  })
+  login(){
+    if(this.loginForm.invalid){
+      return;
+    }
+
+    this._AuthService.login(this.loginForm.value).subscribe(res => {
+      console.log(res);
+    })
+  }
 }
