5a0f5b607508ff2b9621afbede1cc2c28b3b9b51
Amr-Elharery <amrelharery@gmail.com>
2024-07-29 18:52:23 +0300

    Finish movies page

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { TrendingService } from '../services/trending.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-movies',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
+  providers:[TrendingService],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.scss'
 })
 export class MoviesComponent {
-
+  public trendingMovies: any[] = [];
+  constructor(_TrendingService: TrendingService){
+    _TrendingService.getTrending("all").subscribe((response) =>{
+      this.trendingMovies = response.results.filter((trend: any) => trend.media_type === "movie");
+    })
+  }
 }
