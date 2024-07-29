8e25067bb16b49d349df4e59326bff8e3c9675f2
Amr-Elharery <amrelharery@gmail.com>
2024-07-29 18:50:25 +0300

    Finish TV Shows page

--- src/app/tv-shows/tv-shows.component.ts
+++ src/app/tv-shows/tv-shows.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { TrendingService } from '../services/trending.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-tv-shows',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
+  providers : [TrendingService],
   templateUrl: './tv-shows.component.html',
   styleUrl: './tv-shows.component.scss'
 })
 export class TvShowsComponent {
-
+  public trendingShows: any[] = [];
+  constructor(_TrendingService: TrendingService){
+    _TrendingService.getTrending("all").subscribe((response) =>{
+      this.trendingShows = response.results.filter((trend: any) => trend.media_type === "tv");
+    })
+  }
 }
